a3b2d3a9ed0ce7fa4dd104c4a26d9ca7964f7eb7
Никита Высоцкий <nikitavisoc@gmail.com>
2019-11-25 19:00:05 +0300

    +

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,33 +1,82 @@
 import React, { Component } from 'react';
+import axios, { post } from 'axios';
 
 class LoginComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+//  constructor(props) {
+//    super(props);
+//    this.state ={
+//      file:null
+//    }
+//    this.onFormSubmit = this.onFormSubmit.bind(this)
+//    this.onChange = this.onChange.bind(this)
+////    this.fileUpload = this.fileUpload.bind(this)
+//  }
+//  
+//  onFormSubmit(e){
+//    e.preventDefault() // Stop form submit
+//    this.fileUpload(this.state.file).then((response)=>{
+//      console.log(response.data);
+//    })
+//  }
+//
+//  onChange(e) {
+//    this.setState({file:e.target.files[0]})
+//  }
+//
+//  fileUpload(file){
+//    const url = 'http://example.com/file-upload';
+//    const formData = new FormData();
+//    formData.append('file',file)
+//    const config = {
+//        headers: {
+//            'content-type': 'multipart/form-data'
+//        }
+//    }
+//    return  post(url, formData,config)
+//  }
+  
+  state = {
+    name: '',
+  }
+  handleChange = event => {
+    this.setState({ name: event.target.value });
   }
  
-  handleSubmit(e) {
-    alert('The value is: ' + this.input.value);
-    e.preventDefault();
+  handleSubmit = event => {
+    event.preventDefault();
+ 
+    const user= {
+      name: this.state.name
+    };
+    console.log(user);
+    axios.post(`${axios.defaults.baseURL}/people`, { user })
+      .then(res => {
+        console.log(res);
+      })
   }
+  
+  close() {
+    console.log("close");
+  }
+
  
   render() {
     return (
-      <div class="login-container">
-        <div onclick="logPopup('close')" class="login-container__background"></div>
-        <form class="login-container__window" onsubmit="log()">
-          <p class="login-container__title">Войти</p>
-          <div class="login-container__form">
-            <input type="name" id="login" class="login-container__input" maxlength="20" required/>
-            <label for="login" class="login-container__label">Логин</label>
-            <input type="password" id="password" class="login-container__input" maxlength="20" required/>
-            <label for="password" class="login-container__label">пароль</label>
-            <p class="login-container__incorrect">Неверный логин или пароль</p>
-            <a href="#" class="login-container__remember-password">Не помню пароль</a>
+      <div className="login-container">
+        <div onClick={this.close} className="login-container__background"></div>
+        <form className="login-container__window" onSubmit={this.handleSubmit}>
+          <p className="login-container__title">Войти</p>
+          <div className="login-container__form">
+            <input type="name" id="login" className="login-container__input" maxLength="20" onChange={this.handleChange} required/>
+            <label htmlFor="login" className="login-container__label">Логин</label>
+            <input type="password" id="password" className="login-container__input" maxLength="20"/>
+            <label htmlFor="password" className="login-container__label">пароль</label>
+            <p className="login-container__incorrect">Неверный логин или пароль</p>
+            <a href="#" className="login-container__remember-password">Не помню пароль</a>
           </div>
-          <div class="login-container__buttons">
-            <button type="submit" class="login-container__button button login-container__button--login">Войти</button>
-            <a href="#" class="login-container__button button login-container__button--registration">Зарегистрироваться</a>
+          <div className="login-container__buttons">
+            <button type="submit" className="login-container__button button login-container__button--login">Войти</button>
+            <a href="#" className="login-container__button button login-container__button--registration">Зарегистрироваться</a>
           </div>
         </form>
       </div>
