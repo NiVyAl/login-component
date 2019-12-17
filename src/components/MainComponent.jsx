7411875910e6335f2aa0cbeef424f3f263eeeb94
Никита Высоцкий <nikitavisoc@gmail.com>
2019-12-17 08:53:56 +0300

    начал делать responce 200

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class MainComponent extends Component {
     render() {
         return(
-            <p className="main__text">Главная страница</p>
+            <div>
+                <p className="main__text">Главная страница</p>
+                <a href="http://localhost:3000/registrationConfirm?token=aa3ff242-2269">подтверждение email</a>
+            </div>
         )
     }
 }
--- src/components/RegistrationConfirmComponent.jsx
+++ src/components/RegistrationConfirmComponent.jsx
@@ -3,25 +3,37 @@ import axios, { post } from 'axios';
 import url from '../url.js'
 
 class RegistrationConfirmComponent extends Component {
-	// constructor(props)  {
-	// 	super(props);
-	// }
+	constructor(props)  {
+		super(props);
+
+		this.state = {
+			isResponce: false,
+		}
+	}
 	
 	postToken = async event => {
 		let data = window.location.href;
+		let newData = "";
 		for (let i = 0; i < data.length; i++) {
 			if (data[i] === "=") {
-				data = data.slice(i+1, data.length);
+				newData = data.slice(i+1, data.length);
 				break
 			}
 		}
-		console.log(data);
-		try {
-			const response = await axios.post(url, { data });
-			console.log('Returned data:', response);
-		 } catch (e) {
-			console.log(`Axios request failed: ` + e);
-		 }
+		console.log(newData);
+		if (newData !== "") {
+			try {
+				const response = await axios.post(url, { data });
+				const responce2 = 200;
+				this.state.isResponce = responce2;
+				console.log('Returned data:', response);
+			 } catch (e) {
+				const responce2 = 200;
+				this.state.isResponce = responce2;
+				console.log(this.state.isResponce);
+				console.log(`Axios request failed: ` + e);
+			 }
+		}	
 	}
 	
 	render() {
@@ -29,6 +41,16 @@ class RegistrationConfirmComponent extends Component {
 			<div onLoad={this.postToken()}>
 				confirm
 				<button onClick={this.postToken}>Отправить</button>
+				<div className="confirm">
+					<h2 className="sub-title confirm__title">Подтверждение email</h2>
+					{this.state.isResponce &&
+						<p className="confirm__text">email подтвержден!</p>
+					}
+					{this.state.isResponce === false &&
+						<p className="confirm__text">произошла ошибка подтверждения</p>
+					}
+					
+				</div>
 			</div>
 		)
 	}
