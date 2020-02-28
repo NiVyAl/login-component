b2a527973e515131441a4bfb9e263a5d7ba1e20f
Никита Высоцкий <nikitavisoc@gmail.com>
2020-02-28 18:41:25 +0300

    +

--- src/components/ChooseReviewerComponent.jsx
+++ src/components/ChooseReviewerComponent.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import CheckboxComponent from './service/CheckboxComponent';
 
 class ChooseReviewerComponent extends Component {
     constructor(props) {
         super(props);
+        
+        this.state = {
+            isMoreOpen: false,
+            moreButtonText: "Показать всех рецензентов",
+        }
+    }
+    
+    moreOpen = () => {
+        if (this.state.isMoreOpen) {
+            this.setState({ moreButtonText: "Показать всех рецензентов" })
+        } else {
+            this.setState({ moreButtonText: "Показать рецензентов только данной темы" })
+        }
+        this.setState({isMoreOpen: !this.state.isMoreOpen});
     }
 
     render() {
@@ -12,6 +27,38 @@ class ChooseReviewerComponent extends Component {
                 <form className="choose-reviewer modal-window__window" onSubmit={this.handleSubmit}>
                     <p className="modal-window__title sub-title">Выберите рецензентов</p>
                     <p className="modal-window__title-description title-description">{this.props.title}ЭЛЕКТРОХИМИЧЕСКИЙ МЕТОД ИССЛЕДОВАНИЯ ПРОЦЕССОВ МИКРОБНОЙ КОРРОЗИИ</p>
+                    <div class="choose-reviewer__reviewer-container reviewer-container">
+                        <div className="reviewer-container__main">
+                            <CheckboxComponent name="reviewer1" text="Игорь Николаевич Маресьев"/>
+                            <CheckboxComponent name="reviewer2" text="Игорь Николаевич Маресьев"/>
+                            <CheckboxComponent name="reviewer3" text="Игорь Николаевич Маресьев"/>
+                            <CheckboxComponent name="reviewer4" text="Игорь Николаевич Маресьев"/>
+                            <CheckboxComponent name="reviewer5" text="Игорь Николаевич Маресьев"/>
+                            <CheckboxComponent name="reviewer6" text="Игорь Николаевич Маресьев"/>
+                        </div>
+                        <p className="text-button" onClick={this.moreOpen}>{this.state.moreButtonText}</p>
+                        {this.state.isMoreOpen &&
+                            <div className="reviewer-container__more">
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                                <CheckboxComponent name="reviewer7" text="Игорь Николаевич Маресьев"/>
+                            </div>
+                        }
+                    </div>
                 </form>
             </div>
             
--- src/components/service/CheckboxComponent.jsx
+++ src/components/service/CheckboxComponent.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 class CheckboxComponent extends Component {
 	constructor(props) {
-		
+		super(props);
 	}
 	
 	render() {
 		return(
-			// <input type="checkbox">
-			<div></div>
+			<div className="checbox-container">
+				<input id={this.props.name} type="checkbox" className="checkbox-container__checkbox"/>
+				<label htmlFor={this.props.name} className="checkbox-container__label">{this.props.text}</label>
+			</div>
 		)
 	}
 }
